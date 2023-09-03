f1e51bc5ffaf197a64af65c025a230881d8cfb33
Longyuekong <627784964@qq.com>
2023-09-04 00:28:47 +0800

    完善简历细节

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,31 @@
 import Experience from './components/Experience.vue';
 const userInfo = [
     {
-        leftInfo:'男 / 1996.04',
-        rightInfo:'13649506209',
+        leftInfo: '男 / 1996.04',
+        rightInfo: '13649506209',
     },
     {
-        leftInfo:'本科 / 北方民族大学',
-        rightInfo:'627784964@qq.com',
+        leftInfo: '本科 / 北方民族大学',
+        rightInfo: '627784964@qq.com',
     },
     {
-        leftInfo:'2014年毕业 / 4年工作经验',
-        rightInfo:'wyresume.top',
+        leftInfo: '2018年毕业 / 5年工作经验',
+        rightInfo: 'wyresume.top',
     },
-    
+
 ]
 
 // 工作与技能
 const jobSkillConfig = [
     {
-        title:'工作经历',
-        experienceName:'武汉佰钧成技术有限责任公司',
-        workTime:'2019.10 - 至今',
-        tag:'在职',
-        detail:[
+        title: '工作经历',
+        experienceName: 'OPPO',
+        workTime: '2019.10 - 至今',
+        tag: '在职',
+        detail: [
             {
-                detailTitle:'工作内容',
-                descArr:[
+                detailTitle: '工作内容',
+                descArr: [
                     '负责OPPO内容审核，审核标准平台，考试平台的搭建、交互开发、维护和优化工作',
                     '负责OPPO隐私安全平台以及广告负反馈H5（To C）的搭建、交互开发、维护和优化工作',
                     '负责OPPO主题快应用小游戏审核平台的重构'
@@ -35,28 +35,28 @@ const jobSkillConfig = [
         ]
     },
     {
-        title:'',
-        experienceName:'广州潮盟网络科技有限公司',
-        workTime:'2018.10 - 2019.08',
-        tag:'',
-        detail:[
+        title: '',
+        experienceName: '广州潮盟网络科技有限公司',
+        workTime: '2018.10 - 2019.08',
+        tag: '',
+        detail: [
             {
-                detailTitle:'工作内容',
-                descArr:[
+                detailTitle: '工作内容',
+                descArr: [
                     '负责公司客户管理模块的开发与维护',
                 ]
             }
         ]
     },
     {
-        title:'个人能力',
-        experienceName:'Web',
-        workTime:'',
-        tag:'熟悉',
-        detail:[
+        title: '个人能力',
+        experienceName: 'Web',
+        workTime: '',
+        tag: '熟悉',
+        detail: [
             {
-                detailTitle:'',
-                descArr:[
+                detailTitle: '',
+                descArr: [
                     '熟练掌握Vue(2x，3x)框架以及周边生态库',
                     '熟悉HTML5，CSS3，JavaScript(ES5/ES6/ES7)',
                     '熟悉git使用，多人协作开发流程，抽离业务逻辑，封装公共组件'
@@ -64,24 +64,55 @@ const jobSkillConfig = [
             }
         ]
     },
+    // {
+    //     title: '',
+    //     experienceName: 'Web',
+    //     workTime: '',
+    //     tag: '了解',
+    //     detail: [
+    //         {
+    //             detailTitle: '',
+    //             notSort: true,
+    //             descArr: [
+    //                 '了解React设计思想，HTTP协议，Node.js等技术',
+    //             ]
+    //         }
+    //     ]
+    // },
     {
-        title:'',
-        experienceName:'Web',
-        workTime:'',
-        tag:'了解',
-        detail:[
+        title: '项目经历',
+        experienceName: '隐私安全平台',
+        workTime: '',
+        tag: 'Vue3 + Vite',
+        detail: [
             {
-                detailTitle:'',
-                descArr:[
-                    '了解React设计思想，HTTP协议，Node.js等技术',
+                detailTitle: '项目描述',
+                notSort: true,
+                descArr: [
+                    '基本AI自动化能力，为开发者提供专业的隐私检测服务',
                 ]
-            }
+            },
+            {
+                detailTitle: '技术选型',
+                notSort: true,
+                descArr: [
+                    'Vue3 + Vite +  VueX + Element-Plus等',
+                ]
+            },
+            {
+                detailTitle: '主要工作',
+                descArr: [
+                    '负责产品从0到1的开发，快速搭建平台',
+                    '前端性能优化，提升用户体验',
+                ]
+            },
         ]
     },
+   
 ]
 // OPPO内容审核 审核标准平台 考试平台
 // 三品牌全业务内容质量与安全工作，通过标准，流程，审核能力等多种手段，确保内容安全零风险
-// 隐私安全平台
+// 
 // 基本AI和自动化能力，为开发者提供专业的隐私检测服务，协助开发者进行低成本高效率的隐私安全检测，实时监控最新隐私政策，建立开发者快速反馈绿色通道，为创建绿色应用生态专业护航
 // OPPO 广告负反馈H5
 // 负责全球APK（手机，电视，手表）主题快应用小游戏相关的审核能力开发，保障数据的准确性，全链路的审核效率和质量
@@ -89,70 +120,129 @@ const jobSkillConfig = [
 // 为所有接入审核平台的业务制定安全合规，符合业务诉求可落地的执行标准，建立完善的全球内容安全管理体系，提高员工内容安全意识级别风险。消除内容安全风险
 // 项目经历
 const experienceConfig = [
-    {
-        title:'项目经历',
-        experienceName:'OPPO',
-        workTime:'2018.5 - 2019.09',
-        tag:'在职',
-        detail:[
+{
+        title: '项目经历',
+        experienceName: 'OPPO资源后台重构',
+        workTime: '',
+        tag: 'Vue3 Vite',
+        detail: [
             {
-                detailTitle:'项目描述',
-                descArr:[
-                    '1111',
-                    '2222',
-                    '33333'
+                detailTitle: '项目描述',
+                notSort: true,
+                descArr: [
+                    '负责全球APK（手机，电视，手表）主题快应用小游戏相关的审核能力开发，提升全链路审核效率和质量',
                 ]
             },
             {
-                detailTitle:'主要工作',
-                descArr:[
-                    '1111',
-                    '2222',
-                    '33333'
+                detailTitle: '技术选型',
+                notSort: true,
+                descArr: [
+                    'Vue3x + Vite + Element-Plus + VueX等',
+                ]
+            },
+            {
+                detailTitle: '主要工作',
+                descArr: [
+                    '前后端分离，业务逻辑解耦',
+                    '设计公共组件，高质量复用',
+                    '正常迭代更新，优化代码结构'
+                ]
+            },
+        ]
+    },
+    {
+        title: '',
+        experienceName: 'OPPO内容审核 审核标准平台 考试平台',
+        workTime: '',
+        tag: 'Vue2',
+        detail: [
+            {
+                detailTitle: '项目描述',
+                notSort: true,
+                descArr: [
+                    '管理三品牌全业务内容质量与安全工作，通过标准，流程，审核能力等多种手段，确保内容安全零风险',
+                ]
+            },
+            {
+                detailTitle: '技术选型',
+                notSort: true,
+                descArr: [
+                    'Vue2 + VueX + Element + Vue-router等',
+                ]
+            },
+            {
+                detailTitle: '主要工作',
+                descArr: [
+                    '参与平台搭建，负责多个模块，满足业务方审核需求',
+                    '采用模板动态配置方式，快速支持业务接入审核',
                 ]
             },
         ]
     },
 
+    {
+        title: '',
+        experienceName: 'OPPO广告负反馈H5',
+        workTime: '',
+        tag: 'Vant Vue3 Vite',
+        detail: [
+            {
+                detailTitle: '项目描述',
+                notSort: true,
+                descArr: [
+                    '收集用户反馈问题，推进业务整改，提升用户体验',
+                ]
+            },
+            {
+                detailTitle: '技术选型',
+                notSort: true,
+                descArr: [
+                    'Vue3x + Vite + Vant + VueX + Element-Plus等',
+                ]
+            },
+            {
+                detailTitle: '主要工作',
+                notSort: true,
+                descArr: [
+                    '高保真还原UI设计稿，支持移动端适配',
+                ]
+            },
+        ]
+    },
+ 
+
 ]
-const exportPDF = ()=>{
+const exportPDF = () => {
     window.print()
 }
 </script>
 
 <template>
+    <div class="container">
     <div class="export-btn" @click="exportPDF">生成PDF</div>
-<div class="container">
-    <div  class="section">
-     <!-- 个人信息 -->
-     <div class="header">
-        <div class="title" >
-            <div class="name">吴洋</div>
-            <div class="job">Web前端开发工程师 | 深圳</div>
-        </div>
-        <!-- 基本信息 -->
-        <div 
-        v-for="(item,index) in userInfo"
-        :key="index"
-        class="user-info"
-         >
-            <div class="left-info">{{ item.leftInfo }}</div>
-            <div class="right-info">{{ item.rightInfo }}</div>
-        </div>
-    </div>
-    <!-- 工作经历+项目经历 -->
-    <div class="content">
-        <div >
-        <Experience :experienceConfig="jobSkillConfig"></Experience>
-        </div>
-        <div >
-        <Experience :experienceConfig="experienceConfig"></Experience>
+        <div class="section">
+            <!-- 个人信息 -->
+            <div class="header">
+                <div class="title">
+                    <div class="name">吴洋</div>
+                    <div class="job">Web前端开发工程师 | 深圳</div>
+                </div>
+                <!-- 基本信息 -->
+                <div v-for="(item, index) in userInfo" :key="index" class="user-info">
+                    <div class="left-info">{{ item.leftInfo }}</div>
+                    <div class="right-info">{{ item.rightInfo }}</div>
+                </div>
+            </div>
+            <!-- 工作经历+项目经历 -->
+            <div class="content">
+                <div>
+                    <Experience :experienceConfig="jobSkillConfig"></Experience>
+                </div>
+                <div>
+                    <Experience :experienceConfig="experienceConfig"></Experience>
+                </div>
+            </div>
         </div>
     </div>
-   </div>
-</div>
 </template>
 
-<style scoped>
-
-</style>
--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -22,16 +22,16 @@ const { experienceConfig } = toRefs(props)
         <div class="work-header">
             <div class="work-box">
                 <div class="work-title">{{ item.experienceName }}</div>
-                <div class="time" v-if="item.workTime">{{ item.workTime }}</div>
+                <div v-if="item.workTime" class="time">{{ item.workTime }}</div>
             </div>
-            <div v-show="item.tag" class="tag">{{ item.tag }}</div>
+            <div v-show="item.tag" class="tag" :class="item.workTime ? 'work-bottom': ''">{{ item.tag }}</div>
         </div>
         <div v-if="item.detail.length" class="detail">
             <div v-for="detailItem in item.detail" :key="detailItem" class="detail-box">
                 <div v-if="detailItem.detailTitle" class="detail-title">{{ detailItem.detailTitle }}：</div>
                 <div v-if="detailItem.descArr.length" class="desc">
                     <div v-for="(descItem, descIndex) in detailItem.descArr" :key="descItem" class="desc-box">
-                        <div>{{ descIndex + 1 }}、</div>
+                        <div v-show="!detailItem.notSort">{{ descIndex + 1 }}、</div>
                         <div>{{ descItem }}</div>
                     </div>
                 </div>
